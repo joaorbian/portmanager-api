d461027f1562cfb9e93d7f8b9a736aee315b9784
progjoao <joaoricardobianchini@hotmail.com>
2023-04-26 18:04:36 -0300

    mais ajustes

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { TechnologyModule } from './technology/technology.module';
 import { LanguageModule } from './language/language.module';
+import { PrismaService } from 'src/database/prisma.service';
 
 @Module({
-  imports: [UserModule, AuthModule, TechnologyModule, LanguageModule],
+  imports: [UserModule, AuthModule, TechnologyModule, LanguageModule,
+  ],
   controllers: [],
-  providers: [],
+  providers: [PrismaService],
 })
 export class AppModule {}
